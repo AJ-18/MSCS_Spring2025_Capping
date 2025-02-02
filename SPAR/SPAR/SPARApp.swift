eaaf93c228b5a5d58e5c52244cdb99ba4fd5ef5a
AbhijeetCherungottil1 <Abhijeet.Cherungottil1@marist.edu>
2025-02-02 12:33:42 -0500

    Initial setup for iOS Application

---
+++ SPAR/SPAR/SPARApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPARApp.swift
+//  SPAR
+//
+//  Created by Abhijeet Cherungottil on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SPARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
