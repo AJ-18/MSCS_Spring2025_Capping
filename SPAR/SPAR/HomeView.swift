a55ffec42c1f0c9eb5cbfa2ab2a713dcffdab767
AbhijeetCherungottil1 <Abhijeet.Cherungottil1@marist.edu>
2025-02-20 10:10:10 -0500

    Added Initial Onboarding screenview

---
+++ SPAR/SPAR/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  SPAR
+//
+//  Created by Abhijeet Cherungottil on 2/20/25.
+//
+
+import SwiftUI
+
+// Dummy Home screen
+struct HomeView: View {
+    var body: some View {
+        Text("Welecome to Home Screen")
+            .font(.title)
+            .fontWeight(.heavy)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ SPAR/SPAR/OnBoardingScreenView.swift
@@ -0,0 +1,49 @@
+//
+//  OnBoardingScreenView.swift
+//  SPAR
+//
+//  Created by Abhijeet Cherungottil on 2/20/25.
+//
+
+import SwiftUI
+
+struct OnBoardingScreenView: View {
+    let imageName: String
+    let title: String
+    let description: String
+    var showButton: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 300)
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            Text(description)
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 40)
+            
+            if showButton {
+                NavigationLink(destination: HomeView()) {
+                    Text("Get Started")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.horizontal, 40)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    OnBoardingScreenView(imageName: "", title: "Monitor Your System Health Anytime!", description: "Keep an eye on your system’s performance with real-time metrics at your fingertips")
+}
